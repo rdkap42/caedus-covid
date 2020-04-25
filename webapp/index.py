dca513c5c5931034bfcaaabf00dd5d7ea2036886
Tony Carnevale <acarnevale@protonmail.com>
2020-04-25 10:38:35 -0400

    added route for /products

--- webapp/index.py
+++ webapp/index.py
@@ -18,5 +18,9 @@ def about():
 def data():
     return render_template('data_models.html')
 
+@server.route("/products")
+def products():
+    return render_template('products.html')
+
 if __name__ == '__main__':
     server.run( host='0.0.0.0', port=os.environ.get('PORT',8050))
